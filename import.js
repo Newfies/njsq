b4c889c6b4f7fa6869fe789564d8395d192d6793
Newfie <157941859+Newfies@users.noreply.github.com>
2024-03-21 12:15:51 -0500

    Test

---
+++ import.js
@@ -0,0 +1,21 @@
+// Using NodeJS Quick By @Newfies On GitHub!
+// Import This With The Following:
+// const njsQuick = require("https://raw.githubusercontent.com/Newfies/nodejs-quick/main/import.js");
+
+function l(msg) {
+  console.log(msg);
+}
+
+// Coloring Text - Console
+const colorRed = "\x1b[31m";
+const colorGreen = "\x1b[32m";
+const colorBlue = "\x1b[34m";
+const colorYellow = "\x1b[33m";
+const colorBlack = "\x1b[30m:";
+const colorMagenta = "\x1b[35m:";
+const colorCyan = "\x1b[36m:";
+const colorWhite = \x1b[37m:";
+// Coloring Reset - Console
+const colorReset = "\x1b[0m";
+
+module.exports = exports;
