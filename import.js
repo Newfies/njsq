d39a76f81c39f012cf83d4b50182e9ef2218ade0
Newfie <157941859+Newfies@users.noreply.github.com>
2024-03-21 13:11:51 -0500

    Even More Console Log Decoration

--- import.js
+++ import.js
@@ -3,33 +3,71 @@ function l(msg) {
   console.log(msg);
 };
 
-// Coloring Text [Console Log]
-const colorRed = "\x1b[31m";
-const colorGreen = "\x1b[32m";
-const colorBlue = "\x1b[34m";
-const colorYellow = "\x1b[33m";
-const colorBlack = "\x1b[30m";
-const colorMagenta = "\x1b[35m";
-const colorCyan = "\x1b[36m";
-const colorWhite = "\x1b[37m";
-const colorReset = "\x1b[0m";
-
-// Coloring Background of Text [Console Log]
-const place = "holder";
-
-// Styling of Text [Console Log]
-const holder = "place";
+// Colors and Styles for Console Log
+const tRed = "\x1b[31m";
+const tGreen = "\x1b[32m";
+const tBlue = "\x1b[34m";
+const tYellow = "\x1b[33m";
+const tBlack = "\x1b[30m";
+const tMagenta = "\x1b[35m";
+const tCyan = "\x1b[36m";
+const tWhite = "\x1b[37m";
+const tReset = "\x1b[0m";
+const tbGray = "\x1b[90m";
+const tbRed = "\x1b[91m";
+const tbGreen = "\x1b[92m";
+const tbYellow = "\x1b[93m";
+const tbBlue = "\x1b[94m";
+const tbMagenta = "\x1b[95m";
+const tbCyan = "\x1b[96m";
+const tbWhite = "\x1b[97m";
+const bgBlack = "\x1b[40m";
+const bgRed = "\x1b[41m";
+const bgGreen = "\x1b[42m";
+const bgYellow = "\x1b[43m";
+const bgBlue = "\x1b[44m";
+const bgMagenta = "\x1b[45m";
+const bgCyan = "\x1b[46m";
+const bgWhite = "\x1b[47m";
+const tsBold = "\x1b[1m";
+const tsDim = "\x1b[2m";
+const tsUnderline = "\x1b[4m";
+const tsBlink = "\x1b[5m";
+const tsReverse = "\x1b[7m";
+const tsHidden = "\x1b[8m";
 
 // Export Modules
 module.exports = {
   l,
-  colorRed,
-  colorGreen,
-  colorBlue,
-  colorYellow,
-  colorBlack,
-  colorMagenta,
-  colorCyan,
-  colorWhite,
-  colorReset
+  tRed,
+  tGreen,
+  tBlue,
+  tYellow,
+  tBlack,
+  tMagenta,
+  tCyan,
+  tWhite,
+  tReset,
+  tbGray,
+  tbRed,
+  tbGreen,
+  tbYellow,
+  tbBlue,
+  tbMagenta,
+  tbCyan,
+  tbWhite,
+  bgBlack,
+  bgRed,
+  bgGreen,
+  bgYellow,
+  bgBlue,
+  bgMagenta,
+  bgCyan,
+  bgWhite,
+  tsBold,
+  tsDim,
+  tsUnderline,
+  tsBlink,
+  tsReverse,
+  tsHidden
 };
