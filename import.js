196a12c22018ca82fba07fb931bc687315ee05cc
Newfie <157941859+Newfies@users.noreply.github.com>
2024-03-21 12:53:28 -0500

    Update import.js

--- import.js
+++ import.js
@@ -1,23 +1,17 @@
-// Your Using NodeJS Quick By Newfies On GitHub!
-// Trying to upload to npm soon.
-// For now add to your project folder then use
-// const njsQ = require('./import.js')
-
 // Easy console.log Alternative; I'm Lazy
 function l(msg) {
   console.log(msg);
-}
+};
 
-// Coloring Text - Console
+// Coloring Text
 const colorRed = "\x1b[31m";
 const colorGreen = "\x1b[32m";
 const colorBlue = "\x1b[34m";
 const colorYellow = "\x1b[33m";
-const colorBlack = "\x1b[30m:";
-const colorMagenta = "\x1b[35m:";
-const colorCyan = "\x1b[36m:";
-const colorWhite = "\x1b[37m:";
-// Coloring Reset - Console
+const colorBlack = "\x1b[30m";
+const colorMagenta = "\x1b[35m";
+const colorCyan = "\x1b[36m";
+const colorWhite = "\x1b[37m";
 const colorReset = "\x1b[0m";
 
 // Export Modules
